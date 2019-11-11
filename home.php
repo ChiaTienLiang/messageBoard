1a58d12e7bb3585d10598d014b5e11e1ac9dbc35
ChiaTienLiang <krad823@gmail.com>
2019-11-12 00:01:19 +0800

    sql

--- home.php
+++ home.php
@@ -8,28 +8,12 @@
     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
 
     <title>Fixed Top Navbar Example for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="navbar-fixed-top.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <style>
         .message {
             background-color: lightgray;
@@ -39,6 +23,9 @@
 
         h1 {
             text-align: center;
+        }
+        .day{
+            text-align: right;
         }
     </style>
 </head>
@@ -61,8 +48,8 @@
             <div id="navbar" class="navbar-collapse collapse">
 
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="login.html">Sign in<span class="sr-only"></span></a></li>
-                    <li><a href="signUp.html">Sign up</a></li>
+                    <li class="active"><a href="login.php">Sign in<span class="sr-only"></span></a></li>
+                    <li><a href="signUp.php">Sign up</a></li>
                 </ul>
             </div>
             <!--/.nav-collapse -->
@@ -87,18 +74,7 @@
                 <h5>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work.
                     It
                     includes the responsive CSS and HTML, so it also adapts to your viewport and device.</h5>
-                <h5>To see the difference between static and fixed top navbars, just scroll.</h5>
-            </div>
-            <p></p>
-            <div class="message">
-                <h3>Navbar example</h3>
-                <hr>
-                <h5>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work.
-                    It
-                    includes the responsive CSS and HTML, so it also adapts to your viewport and device.</h5>
-
-                <h5>2019-11-11</h5>
-
+                <h5 class="day">2019-11-11</h5>
             </div>
             <p></p>
             <div class="message">
@@ -106,7 +82,7 @@
                 <h5>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work.
                     It
                     includes the responsive CSS and HTML, so it also adapts to your viewport and device.</h5>
-                <h5>To see the difference between static and fixed top navbars, just scroll.</h5>
+                <h5 class="day">2019-11-11</h5>
             </div>
             <p></p>
             <div class="message">
@@ -114,7 +90,7 @@
                 <h5>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work.
                     It
                     includes the responsive CSS and HTML, so it also adapts to your viewport and device.</h5>
-                <h5>To see the difference between static and fixed top navbars, just scroll.</h5>
+                <h5 class="day">2019-11-11</h5>
             </div>
             <p></p>
             <div class="message">
@@ -122,13 +98,21 @@
                 <h5>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work.
                     It
                     includes the responsive CSS and HTML, so it also adapts to your viewport and device.</h5>
-                <h5>To see the difference between static and fixed top navbars, just scroll.</h5>
+                <h5 class="day">2019-11-11</h5>
+            </div>
+            <p></p>
+            <div class="message">
+                <h3>Navbar example</h3>
+                <h5>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work.
+                    It
+                    includes the responsive CSS and HTML, so it also adapts to your viewport and device.</h5>
+                <h5 class="day">2019-11-11</h5>
             </div>
             <div class="jumbotron p-3 p-md-5 ">
                 <div>
                     <h1 class="display-4 font-italic">input your message here</h1>
                     <div class="form-group">
-                       <textarea class="form-control" style="width:100%" rows="7"></textarea>
+                        <textarea class="form-control" style="width:100%" rows="7"></textarea>
                     </div>
                 </div>
                 <button class="btn btn-lg  btn-default btn-block" type="submit"
@@ -141,10 +125,11 @@
     ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
         <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+        <script>
+            window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')
+        </script>
         <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-        <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -12,17 +12,13 @@
     integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"> -->
   <!-- Bootstrap core CSS -->
   <!-- <link href="/docs/4.2/dist/css/bootstrap.css" rel="stylesheet"> -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
-    integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
   <!-- Optional theme -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css"
-    integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
 
   <!-- Latest compiled and minified JavaScript -->
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
-    integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
-    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 
   <style>
     .bd-placeholder-img {
@@ -47,11 +43,12 @@
 </head>
 
 <body class="text-center">
-  <form class="form-signin">
-
+  <form class="form-signin" action="loginCheck.php" method='get'>
     <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+    <br>
     <label for="inputEmail" class="sr-only">Email address</label>
     <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+    <br>
     <label for="inputPassword" class="sr-only">Password</label>
     <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
     <div class="checkbox mb-3">
